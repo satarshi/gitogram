b7f44faa239f51583f17595a24cd810703a1f753
“satarshi” <satarshi@mts.ru>
2023-11-27 10:05:34 +0300

    sliderItem

---
+++ src/components/sliderItem/sliderItem.vue
@@ -0,0 +1,77 @@
+<template>
+  <div class="wrapper">
+    <Progress />
+    <div class="avatar-container">
+      <Avatar
+        avatar="https://cdn-icons-png.flaticon.com/512/146/146013.png"
+        username="Peter"
+      />
+    </div>
+    <div class="content-wrapper">
+      <slot />
+    </div>
+    <div class="button-wrapper">
+      <Button title="Follow" />
+    </div>
+  </div>
+</template>
+  
+<script>
+import { progress } from '../progress'
+import { avatar } from '../avatar'
+import { button } from '../button'
+
+export default {
+  name: 'sliderItem',
+  components: {
+    Progress: progress,
+    Avatar: avatar,
+    Button: button
+  }
+}
+</script>
+
+<styles lang="scss" scoped>
+.wrapper {
+  padding: 8px;
+  width: 375px;
+  border-radius: 8px;
+  background: #FFFFFF;
+}
+
+.avatar-container {
+  margin-top: 12px;
+  margin-bottom: 12px;
+  margin-left: 4px;
+}
+
+.content-wrapper {
+  padding: 0px 10px;
+  color: #404040;
+  font-size: 14px;
+  font-weight: 400;
+  line-height: 22.40px;
+
+  &:before {
+    display: block;
+    content: '';
+    background-color: #CACACA;
+    height: 1px;
+    margin: 0px -8px;
+  }
+
+  &:after {
+    display: block;
+    content: '';
+    background-color: #CACACA;
+    height: 1px;
+    margin: 0px -8px;
+  }
+}
+
+.button-wrapper {
+  padding: 24px 0px;
+  display: flex;
+  justify-content: center;
+}
+</styles>
