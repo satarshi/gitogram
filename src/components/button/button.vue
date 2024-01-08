c3a7be6fb944d9c10d3d85d6fa814885246a3890
“satarshi” <satarshi@mts.ru>
2024-01-08 11:37:28 +0300

    feat: доработка компонента кнопки

--- src/components/button/button.vue
+++ src/components/button/button.vue
@@ -1,13 +1,39 @@
 <template>
-  <button class="button">{{ title }}</button>
+  <button class="button" :class="[ theme, { loading: loading }, { disabled: disabled } ]">
+    <template  v-if="loading">
+      <div class="spinner">
+        <icon name="spinner" />
+      </div>
+    </template>
+    <template v-else>
+      {{ title }}
+    </template>
+  </button>
 </template>
 
 <script>
+import { icon } from '@/icons'
+
 export default {
   name: 'button',
+  components: {
+    icon
+  },
   props: {
     title: {
       type: String
+    },
+    theme: {
+      type: String,
+      default: 'green'
+    },
+    loading: {
+      type: Boolean,
+      default: false
+    },
+    disabled: {
+      type: Boolean,
+      default: false
     }
   }
 }
@@ -16,14 +42,39 @@ export default {
 <style lang="scss" scoped>
 .button {
   padding: 11px 109px;
-  background: #31AE54;
   border-radius: 5px;
   color: white;
   font-size: 16px;
   font-weight: 700;
 
-  &:hover {
+  &.green {
+    background: #31AE54;
+
+    &:hover {
+      background: #9E9E9E;
+    }
+  }
+
+  &.gray {
     background: #9E9E9E;
+
+    &:hover {
+      background: #31AE54;
+    }
+  }
+
+  .spinner {
+    height: 16px;
+    width: 16px;
+    color: white;
+  }
+
+  &.loading {
+    pointer-events: none;
+  }
+
+  &.disabled {
+    pointer-events: none;
   }
 }
 </style>
